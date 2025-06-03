9eab0bcecfdc09ff48986cc91eeaf97ebdbdce2e
omarazzawi <omar.alazzawi1987@gmail.com>
2025-06-03 22:15:08 +0200

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
+    
+
 
 });
 
@@ -31,7 +33,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+
+    } else if(gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -72,7 +79,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x")
+        return [operand1 * operand2, "multiply"];
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -106,9 +115,12 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
+    
 
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
